ec20aa0f09b25f0d2fa0bf525c67570614948afa
ruby <as97108@gmail.com>
2023-03-27 14:49:00 +0800

    fix : group connect line bug

--- src/Mode/GeneralizationMode.java
+++ src/Mode/GeneralizationMode.java
@@ -9,27 +9,32 @@ public class GeneralizationMode extends LineMode {
 	
 	public void mousePressed(MouseEvent e) {
 		getFromPort(e.getPoint());
-		tempPort = new Port(e.getX(), e.getY());
-		tempLine = new GeneralizationLine(fromPort, tempPort);
-		canva.addShape(tempLine);
-		canva.repaint();
+		if(canva.getSelected() != null) {
+			tempPort = new Port(e.getX(), e.getY());
+			tempLine = new GeneralizationLine(fromPort, tempPort);
+			canva.addShape(tempLine);
+			canva.repaint();
+		}
 	}
 	
 	public void mouseDragged(MouseEvent e) {
-		tempPort.setPoint(e.getPoint());
-		canva.repaint();
+		if(canva.getSelected() != null) {
+			tempPort.setPoint(e.getPoint());
+			canva.repaint();
+		}
 	}
 	
 	public void mouseReleased(MouseEvent e) {
-		
-		canva.getShapeList().remove(tempLine);
-		shapeList = canva.getShapeByPoint(e.getPoint());
-		if(shapeList.size() != 0) {
-			if(shapeList.get(0) != canva.getSelected()) {
-				toPort = shapeList.get(0).getClosetPort(e.getPoint().x, e.getPoint().y);
-				canva.addShape(new GeneralizationLine(fromPort, toPort));
-			}	
+		if(canva.getSelected() != null) {
+			canva.getShapeList().remove(tempLine);
+			shapeList = canva.getShapeByPoint(e.getPoint());
+			if(shapeList.size() != 0 && !shapeList.get(0).isGroup()) {
+				if(shapeList.get(0) != canva.getSelected()) {
+					toPort = shapeList.get(0).getClosetPort(e.getPoint().x, e.getPoint().y);
+					canva.addShape(new GeneralizationLine(fromPort, toPort));
+				}	
+			}
+			canva.repaint();
 		}
-		canva.repaint();
 	}
 }
--- src/Mode/LineMode.java
+++ src/Mode/LineMode.java
@@ -17,11 +17,12 @@ public class LineMode extends Mode {
 	
 	public void getFromPort(Point p) {
 		shapeList = canva.getShapeByPoint(p);
-		if (shapeList.size() != 0 /* && !shapeList.get(0).isGroup() */) {
+		if (shapeList.size() != 0 && !shapeList.get(0).isGroup()) {
 			canva.setSelected(shapeList.get(0));
 			fromPort = canva.getSelected().getClosetPort(p.x, p.y);
 		} else {
 			canva.setSelected(null);
 		}
 	}
+	
 }
